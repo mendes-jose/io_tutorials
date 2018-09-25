cf2300aa4561aec5648cee075beef025a7aa40d2
José Magno MENDES FILHO <mendesfilho@protonmail.com>
2018-09-25 08:02:37 +0200

    Fix chatter

--- dec_mrs/chatter.py
+++ dec_mrs/chatter.py
@@ -9,24 +9,25 @@ def getInfoCB(data):
     rospy.loginfo(rospy.get_caller_id() + ' I heard %s', data.data)
 
 
-def talker():
+def chatter():
     pub = rospy.Publisher(os.getenv('MY_ID') + '_telemetry', String, queue_size=10)
     rospy.init_node('chatter', anonymous=True)
 
     comm_list = os.getenv('COMM_LIST')
-    comm_list.split(":")
+    comm_list = comm_list.split(":")
     for id in comm_list:
         rospy.Subscriber(id + '_telemetry', String, getInfoCB)
 
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         hello_str = "hello_world from " + os.getenv('MY_ID') + " at %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
+        # rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
 
 if __name__ == '__main__':
+    rospy.loginfo(rospy.get_caller_id() + ' Start')
     try:
-        talker()
+        chatter()
     except rospy.ROSInterruptException:
         pass
